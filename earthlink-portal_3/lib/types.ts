8c9f66cfe86ab01d3b195278f4c90aa5ee83902e
rajbirgsingh12-boop <rajbirgsingh12@gmail.com>
2026-07-19 20:35:54 -0400

    Add files via upload

--- earthlink-portal_3/lib/types.ts
+++ earthlink-portal_3/lib/types.ts
@@ -5,4 +5,5 @@ export interface Release {
   id: string; contract_id: string; rel_number: string; location: string; buildings: string;
   ticket: string; amount: number; pre_check: string; date_completed: string;
   payroll_done: boolean; received: boolean; canceled: boolean; labor_hours: number; assigned_to: string | null;
+  labor_breakdown?: { cls: string; hours: number }[] | null; address?: string;
 }
